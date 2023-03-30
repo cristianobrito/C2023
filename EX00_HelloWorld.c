10edc9a8def46aba94470994ae9e06b444893d6e
cristiano oliveira <britonano@gmail.com>
2023-03-30 10:54:37 -0300

    iniciando os estudos avancados

---
+++ EX00_HelloWorld.c
@@ -0,0 +1,17 @@
+/*  todo programa começa com comentario
+ *  do que ele faz mostra alo mundo
+ *  limite de linhas 70
+ *  identação 2 espacos
+ *  author: cristiano
+ *  data: 30/03/2023
+ *
+ * */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("ALO MUNDO!\n");  
+  return 0;
+}
+
+// gcc EX00_HelloWorld.c
---
+++ HelloWorld.c
@@ -0,0 +1,17 @@
+/*  Todo programa deve começar com apresentaçao
+ *  author cristiano
+ *  data: 30/03/2023
+ *  programa faz ola mundo
+ *  identação 2 espaços
+ *  limite de linhas 70
+ * */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("OLA MUNDO!!\n");  
+  return 0;
+}
+
+// para compilar 
+// gcc HelloWorld.c -o HelloWorld
